7b03c900ecd7e377da8755fdac409dbb16aaa412
MysticalDevil <jinpeng7981@outlook.com>
2023-06-28 12:51:46 +0800

    Some optimizations and adjustments

--- .config/zsh/configures.zsh
+++ .config/zsh/configures.zsh
@@ -7,8 +7,3 @@ source $HOME/.config/broot/launcher/bash/br
 # Eliminate duplicate entries in history
 setopt HIST_IGNORE_DUPS
 
-# Autojump plugin
-# [[ -s /etc/profile.d/autojump.sh ]] && . /etc/profile.d/autojump.sh
-
-# atuin keybind
-# bindkey '^t' _atuin_search_widget
--- .config/zsh/evals.zsh
+++ .config/zsh/evals.zsh
@@ -2,13 +2,13 @@
 # Use zoxide to replace cd command
 eval "$(zoxide init zsh --cmd cd)"
 # Enable starship
-eval "$(starship init zsh)"
+if [[ $TERM != "linux" ]]; then
+    eval "$(starship init zsh)"
+fi
 # Thefuch plugin
 eval "$(thefuck --alias)"
 # navi -- An interactive cheatsheet tool for the command-line
 eval "$(navi widget zsh)"
-# Set opam(OCaml package manager) current env
-eval "$(opam env --switch=default)"
 # fnm -- NodeJS version manager written by Rust
 eval "$(fnm env --use-on-cd)"
 # rbenv -- Simple ruby version manager
