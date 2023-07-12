4c0345bdc23569766316bffd17a7691da0114bd1
MysticalDevil <jinpeng7981@outlook.com>
2023-07-12 11:59:48 +0800

    Add some fish plugin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# ------------------ Third part software ------------------
 # Starship prompt
 function starship_transent_rmpt_func
-  starship module time
+    starship module time
 end
 starship init fish | source
 enable_transience
 
+# atuin -- Magical shell history
+atuin init fish --disable-up-arrow | source
+
+# zoxide -- A smarter cd command
+zoxide init fish --cmd cd | source
+
+# fnm -- fast node manager
+fnm env --use-on-cd | source
+
+# ------------------------- Alias -------------------------
