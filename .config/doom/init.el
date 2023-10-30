de69e83b5d0c30fefab79bf05b88e1803b5c0086
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-10-30 14:14:05 +0800

    Enable some doom emacs modules

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -45,7 +45,7 @@
        (popup +defaults)    ; tame sudden yet inevitable temporary windows
        tabs
        treemacs             ; a project drawer, like neotree but cooler
-       ;; unicode           ; extended unicode support for various languages
+       unicode              ; extended unicode support for various languages
        (vc-gutter +pretty)  ; vcs diff in the fringe
        vi-tilde-fringe      ; fringe tildes to mark beyond EOB
        window-select        ; visually switch windows
@@ -61,16 +61,17 @@
        lispy                ; vim for lisp, for people who don't like vim
        multiple-cursors     ; editing in many places at once
        objed                ; text object editing for the innocent
-       parinfer          ; turn lisp into python, sort of
+       parinfer             ; turn lisp into python, sort of
        rotate-text          ; cycle region at point between text candidates
        snippets             ; my elves. They type so I don't have to
        word-wrap            ; soft wrapping with language-aware indent
 
        :emacs
-       dired                ; making dired pretty [functional]
+       (dired
+        +icons +ranger)     ; making dired pretty [functional]
        electric             ; smarter, keyword-based electric-indent
-       ibuffer              ; interactive buffer management
-       undo                 ; persistent, smarter undo for your inevitable mistakes
+       (ibuffer +icons)     ; interactive buffer management
+       (undo +tree)         ; persistent, smarter undo for your inevitable mistakes
        vc                   ; version-control and Emacs, sitting in a tree
 
        :term
