22ad43d281ecabe5d868f465b62793017caffa0b
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-12-05 10:47:04 +0800

    Add emscripten support

--- .config/zsh/core/configures.zsh
+++ .config/zsh/core/configures.zsh
@@ -1,5 +1,9 @@
 # Broot configure
-source $HOME/.config/broot/launcher/bash/br
+source "$HOME/.config/broot/launcher/bash/br"
+
+# emsdk -- CXX wasm sdk
+export EMSDK_QUIET=1
+source "$HOME/.local/share/emsdk/emsdk_env.sh"
 
 # Eliminate duplicate entries in history
 setopt HIST_IGNORE_DUPS
