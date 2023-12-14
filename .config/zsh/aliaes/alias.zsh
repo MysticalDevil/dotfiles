4c90edd160ca0240bf5cba7ee20a668a12decfa4
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-12-14 21:00:35 +0800

    Add some alias

--- .config/zsh/aliaes/alias.zsh
+++ .config/zsh/aliaes/alias.zsh
@@ -1,2 +1,29 @@
 # ----------------------- Alias program -----------------------------
+
+# tmux
 alias tmux='TERM=xterm-256color tmux'
+
+# python
+alias python=python3
+alias pip=pip3
+
+# programming
+alias gopath="cd $GOPATH/src"
+alias goi="go get"
+alias gor="go run"
+alias gob="go build"
+alias gom="go run main.go"
+alias goinit="go mod init"
+alias rustinit="cargo new"
+
+# project
+alias cls="clear" # 清理终端
+alias ..="cd .." # 返回上一级
+alias 。。="cd .." # 返回上一级
+alias ...="cd ../.." # 返回上上级
+alias 。。。="cd ../.." # 返回上上级
+alias ....="cd ../../.." # 返回上上上级
+alias 。。。。="cd ../../.." # 返回上上上级
+alias link="npm link" # link 本地包
+alias unlink="npm unlink" # unlink 本地包
+alias rmmodule="remove !" # 删除node_modules
